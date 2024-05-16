b5db55bd153e5b9281f0ec18766a521a7409b11f
danteire <pioter814@gmail.com>
2024-05-16 23:59:03 +0200

    Dodano UI z GUZIKAMI:)

--- script.js
+++ script.js
@@ -1,3 +1,97 @@
+const rockBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+
+rockBtn.setAttribute("style","height: 30px;width: 80px;")
+rockBtn.textContent = "ROCK";
+paperBtn.textContent = "PAPER";
+scissorsBtn.textContent = "SCISSORS";
+paperBtn.setAttribute("style","height: 30px;width: 80px;")
+scissorsBtn.setAttribute("style","height: 30px;width: 80px;")
+
+const listaHtml = document.querySelector(".listaWyoru");
+const bodyPointer = document.querySelector("body");
+
+listaHtml.setAttribute("style","display: flex;flex-flow: row wrap;justify-content: center; gap:10px")
+listaHtml.appendChild(rockBtn);
+listaHtml.appendChild(paperBtn);
+listaHtml.appendChild(scissorsBtn);
+
+bodyPointer.appendChild(listaHtml);
+
+const komRunda = document.createElement("div");
+const komunikat = document.createElement("div");
+const komWynikPl = document.createElement("div");
+const komWynikCom = document.createElement("div");
+
+bodyPointer.appendChild(komunikat);
+bodyPointer.appendChild(komWynikPl);
+bodyPointer.appendChild(komWynikCom);
+
+let buttons = [rockBtn, paperBtn,scissorsBtn];
+let iloscKlikniec = 0;
+komRunda.innerText = `Rozpocznij grę nasikając guzik`;
+
+bodyPointer.appendChild(komRunda);
+
+buttons.forEach((button) =>{
+        button.addEventListener("click", () =>{
+            console.log(button.textContent);
+            iloscKlikniec++;
+            
+            playGame(String(button.textContent),iloscKlikniec);
+            
+        })
+})
+
+
+
+
+
+let computer_score = 0;
+let plater_score = 0;
+
+function playGame(przycisk){
+    let playerSelection = przycisk;
+    if(playerSelection==undefined){
+        console.log("Wybierz przycisk:");
+        return 3;
+    }
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    const computerSelection = getComputerChoice();
+    let wynik = playRound(computerSelection, playerSelection);
+        if(iloscKlikniec==5){
+            komRunda.innerText = `Nr Rundy: ${iloscKlikniec}`;
+            
+            koniecGry(plater_score,computer_score);
+        }else if(iloscKlikniec<5){
+            
+            if(wynik.search("win")>=0){
+                plater_score++;
+            }else if(wynik.search("lose")>=0){
+                computer_score++;
+            }
+            komWynikPl.innerText = `Wynik Gracza: ${plater_score}\n`;
+            komWynikCom.innerText = `Wynik Komputera: ${computer_score}\n`;
+
+            komRunda.innerText = `Nr Rundy: ${iloscKlikniec}`;
+            komunikat.innerText =" Wynik: " + wynik;
+        }
+}
+
+function koniecGry(wynikP, wynikK){
+   
+    if(wynikP==wynikK){
+        komunikat.innerText = "Final Result: TIE";
+    }else if(wynikP>wynikK){
+        komunikat.innerText = "Final Result: PLAYER WIN";
+    }else{
+        komunikat.innerText = "Final Result: COMPUTER WIN";
+    }
+ 
+}
+
 function getComputerChoice(){
 
     let wybor_komputer = Math.floor(Math.random()*3);
@@ -29,48 +123,4 @@ function playRound(computerSelection, playerSelection){
             return "mamy blad";
         }
     }
-}
-
-
-function playGame(){
-    let playerSelection = prompt("Wpisz: ROCK PAPER NOZYCE");
-// if(playerSelection != "rock" && playerSelection !="paper" && playerSelection!="scissors"){
-//     console.log("Zly input");
-// }
-playerSelection = playerSelection.toLocaleLowerCase();
-const computerSelection = getComputerChoice();
-let wynik = playRound(computerSelection, playerSelection);
-console.log(wynik);
-    if(wynik.search("win")>=0){
-        return 1;
-    }else if(wynik.search("lose")>=0){
-        return -1;
-    }else if(wynik.search("Tie")>=0){
-        return 0;
-    }
-}
-let computer_score = 0;
-let plater_score = 0;
-for(let i =0; i<5;i++){
-    wynik_gry = playGame();
-if(wynik_gry==1){
-    plater_score++;
-}else if(wynik_gry==0){
-    plater_score++;
-    computer_score++;
-}
-else if(wynik_gry==-1){
-    computer_score++;
-}
-}
-
-console.log(`Wynik Gracza: ${plater_score}\n`);
-console.log(`Wynik Komputera: ${computer_score}\n`);
-
-if(plater_score==computer_score){
-    console.log("Final Result: TIE");
-}else if(plater_score>computer_score){
-    console.log("Final Result: PLAYER WIN");
-}else{
-    console.log("Final Result: COMPUTER WIN");
 }
